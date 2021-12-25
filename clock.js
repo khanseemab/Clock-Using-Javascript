c74b9ddbcda668bb825b09f590e1767e3cf06388
seemab2703@gmail.com <seemab2703@gmail.com>
2021-12-25 16:43:00 +0530

    clock

---
+++ clock.js
@@ -0,0 +1,4 @@
+// a=new Date;
+// let Date=a.toLocaleDateString();
+// let Time=a.getHours()+' : '+a.getMinutes()+' : '+a.getSeconds();
+// document.getElementById('time').innerHTML= date +" on "+ time;
