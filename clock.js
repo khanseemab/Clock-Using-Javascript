c19a34c949b4ba4ec7009ed248d1f07588aec5f8
seemab2703@gmail.com <seemab2703@gmail.com>
2022-02-22 23:59:47 +0530

    updated

--- clock.js
+++ clock.js
@@ -2,3 +2,14 @@
 // let Date=a.toLocaleDateString();
 // let Time=a.getHours()+' : '+a.getMinutes()+' : '+a.getSeconds();
 // document.getElementById('time').innerHTML= date +" on "+ time;
+let a;
+let date;
+let time;
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+setInterval(()=>{
+
+a = new Date();
+  date = a.toLocaleDateString(undefined,options);
+  time = a.getHours() + ' : ' + a.getMinutes() + ' : ' + a.getSeconds();
+  document.getElementById('time').innerHTML = time + "<br> on " + date;
+},100);
